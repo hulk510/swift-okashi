7e9d87a980d3f379a5ed57bd991bbdbd8881ba36
hulk510 <dorian.51069@gmail.com>
2022-02-15 13:59:27 +0900

    Initial Commit

---
+++ swift-okashi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-okashi
+//
+//  Created by 後藤遥 on 2022/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-okashi/swift_okashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_okashiApp.swift
+//  swift-okashi
+//
+//  Created by 後藤遥 on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct swift_okashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
